32ef7457419d8a6fb9bc9d765c7448921987aba5
Takumi Ishiyama <mt.stone20@gmail.com>
2020-09-09 16:13:48 +0900

    Refactoring

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,28 +3,42 @@ class Card < ApplicationRecord
   include CalculationTitleScore
   require 'levenshtein'
 
+  BLANK_REGEXP = /[\s　]/.freeze
+  TITLE_RELEVANCE_THRESHOLD = 80
+  SIMILARITY_DISTANCE_THRESHOLD = 2
+
+  # 名寄せ可能かどうか
+  def aggregatable?(email, name, title)
+    # メールアドレスが類似している かつ (表記ゆれを考慮した名前が一致 または 職業関連度が高い)
+    similar_email?(email) && (same_person_name?(name) || relevant_title?(title))
+  end
+
+  private
+
+  def similar_email?(email)
+    Levenshtein.distance(self.email, email) <= SIMILARITY_DISTANCE_THRESHOLD
+  end
+
+  def relevant_title?(title)
+    calculation(self.title, title) >= TITLE_RELEVANCE_THRESHOLD
+  end
+
+  def same_person_name?(name)
+    name_matched?(name) || last_name_matched?(name)
+  end
+
   def name_matched?(name)
     trim_all_space_char(self.name) == trim_all_space_char(name)
   end
 
-  def lastname_matched?(name)
-    splitted_target_names = self.name.split(/[\s　]/)
-    splitted_names = name.split(/[\s　]/)
+  def last_name_matched?(name)
+    splitted_target_names = self.name.split(BLANK_REGEXP)
+    splitted_names = name.split(BLANK_REGEXP)
 
     return false unless splitted_target_names.size == 2 && splitted_names.size == 2
 
     splitted_target_names.last == splitted_names.last
   end
-
-  def similar_email?(email)
-    Levenshtein.distance(self.email, email) <= 2
-  end
-
-  def similar_title?(title)
-    calculation(self.title, title) >= 80
-  end
-
-  private
 
   def trim_all_space_char(str)
     str.gsub(/[\s　]*/, '')
